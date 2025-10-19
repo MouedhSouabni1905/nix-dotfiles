d65080be945a7cdecd186b03d3236448d21d7d9b
Mouedh Souabni <mouedhsmsouabni@gmail.com>
2025-10-19 21:24:20 +0200

    Configured network routing to lp board

--- network.nix
+++ network.nix
@@ -1,19 +1,50 @@
 {config, pkgs, ...}:
 
 {
+        boot.kernel.sysctl = {
+                "net.ipv4.ip_forward" = true;
+                "net.ipv6.conf.all.forwarding" = true;
+        };
 
         networking = {
-                useNetworkd = true;
 
-                bridges.br0.interfaces = ["enp46s0" "wlp45s0"];
+                hostName = "nixos";
 
-                networking.useDHCP = false;
 
-                networking.interfaces.br0.useDHCp = true;
+                interfaces.enp46s0 = {
+                        useDHCP = false;
+                        ipv4.addresses = [{
+                                address = "198.168.10.1";
+                                prefixLength = 24;
+                        }];
+                };
 
-                interface.br0.ipv4.addresses = [
-                        { address = "192.168.1.100"; prefixLength = 24; }
-                ];
+                nat = {
+                        enable = true;
+                        externalInterface = "wlp45s0";
+                        internalInterfaces = [ "enp46s0" ];
+                };
+
+                firewall = {
+                        enable = true;
+                        allowedUDPPorts = [ 53 67 ]; # 53 for DNS, 67 for DHCP
+                        allowedTCPPorts = [ 80 443 22 ];
+                        allowPing = true;
+                };
+
+        };
+
+
+        services.dnsmasq = {
+                enable = true;
+                resolveLocalQueries = false;
+                settings = {
+                        interface = "enp46s0";
+                        "dhcp-range" = [ "198.168.10.100,198.168.10.200,255.255.255.0,24h" ];
+                        "dhcp-authoritative" = true;
+                        "no-resolv" = true;
+                        "server" = [ "1.1.1.1" "8.8.8.8" ];
+                };
         };
 
 }
